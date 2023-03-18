4d0528205434f31ba0d0dff7a8a75189d8eb3d7b
unknown <zhelev.alexandar@gmail.com>
2023-03-19 00:58:23 +0200

    fuel calculator

---
+++ index.js
@@ -0,0 +1,13 @@
+function calculateConsumption(){
+    let distance = document.getElementById('distance').value;
+    let fuel = document.getElementById('fuel').value;
+    let price = document.getElementById('price').value;
+
+    let consumed = (fuel / (distance/100)).toFixed(2);
+    let pricePer100km = (consumed * price).toFixed(2);
+
+    document.getElementById('result').innerHTML = 
+        "Consumption is " + consumed + 
+        " Litres/100km ( $" + pricePer100km + " / 100km ) <br><br>Total Fuel cost = " 
+        + (fuel * pricePer100km).toFixed(2);
+    }
